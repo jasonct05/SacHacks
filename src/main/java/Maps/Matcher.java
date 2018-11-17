bd3258459d778ee4cda73e3a07e30997803e3368
Jason Tan <jct96@cs.washington.edu>
2018-11-17 14:10:36 -0800

    framework for maps api

--- src/main/java/Maps/Matcher.java
+++ src/main/java/Maps/Matcher.java
@@ -3,13 +3,26 @@ import Model.Driver;
 import Model.Event;
 import Model.Rider;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Matcher {
     public static final int MAX_DISTANCE = 10; // in miles
 
-    public static Map<Driver, List<Rider>> findClusters(List<Driver> lDriver, List<Rider> lRider) {
+    public static Map<Driver, Set<Rider>> findRidersInRegion(List<Driver> lDriver, List<Rider> lRider) {
+        // initialize
+        Map<Driver, Set<Rider>> result = new HashMap<>();
+        for(Driver d : lDriver) {
+            result.put(d, new HashSet<>());
+        }
+
+        // shitty O(n^2) alg but fuck it
+        for(Rider r : lRider) {
+            for ()
+        }
+       return null;
+    }
+
+    public static Map<Driver, List<Rider>> findClusters(Map<Driver, Set<Rider>> driverRiderMappingInRegion) {
         //TODO: @Alex call maps api to get clusters;
         return null;
     }
