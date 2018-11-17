5be13325514505fc2be627d53bc1a82050bccf6f
Jason Tan <jct96@cs.washington.edu>
2018-11-17 13:45:17 -0800

    no longer mad at lombok

--- src/main/java/Model/Driver.java
+++ src/main/java/Model/Driver.java
@@ -2,9 +2,12 @@ package Model;
 
 import lombok.Data;
 
-@Data
 public class Driver extends User {
     public final int maxSeats;
     public final int availableSeats;
     public final String meetupLocation;
+
+    public Driver() {
+        super();
+    }
 }
