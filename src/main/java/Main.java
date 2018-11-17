d022e9f2a38dc411d113a832a450b013f1b18bc9
Jason Tan <jct96@cs.washington.edu>
2018-11-17 13:41:52 -0800

    completed mock

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,13 @@
 import Mocks.BatchingModelInitializer;
 
+import java.net.ServerSocket;
+
 public class Main {
     public static void main(String[] args) {
         Model.BatchingModel bm = BatchingModelInitializer.initializeEventModel();
 
         // TODO: Set up connections singleton here
-        
+        ServerSocket ss = Networks.TCPServer.getTCPServerSocket();
         while (true) {
             // if receive connection on tcp port
                 // establish new thread
