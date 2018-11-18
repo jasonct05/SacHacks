edf2cd8868750b5a350bafc8956f7d55c7f4108d
kschiang19 <kschiang19@gmail.com>
2018-11-17 19:57:41 -0800

    added driver and riders

--- src/main/java/Mocks/DriverAndRiderMock.java
+++ src/main/java/Mocks/DriverAndRiderMock.java
@@ -22,6 +22,16 @@ public class DriverAndRiderMock {
                 4,
                 "Outside the Stadium"
             ));
+        s.add(new Driver(
+                "Alex Chiang",
+                "2",
+                "Hi I am a sacramento kings fan",
+                new Location(38.6870, -121.3069),
+                LocalDateTime.of(2018, 11, 21, 15, 00),
+                4,
+                4,
+                "Outside the Stadium"
+        ));
         return s;
     }
 
@@ -29,16 +39,51 @@ public class DriverAndRiderMock {
         Set<Rider> s = new HashSet<>();
         s.add(new Rider(
                 "Jason Tan",
-                "2",
+                "3",
                 "Hi I am a sacramento kings fan",
                 new Location(38.6751, -121.3909),
                 LocalDateTime.of(2018, 11, 21, 15, 00)
         ));
         s.add(new Rider(
                 "Stefan Layanto",
-                "3",
-                "Hi I am not a sacramento kings fan",
+                "4",
+                "Hi I am a sacramento kings fan",
                 new Location(38.6393, -121.3797),
+                LocalDateTime.of(2018, 11, 21, 15, 00)
+        ));
+        s.add(new Rider(
+                "De'Aaron Fox",
+                "5",
+                "My favorite player is De'Aaron Fox",
+                new Location(38.6614, -121.3425),
+                LocalDateTime.of(2018, 11, 21, 15, 00)
+        ));
+        s.add(new Rider(
+                "Bogdan Bogdanovic",
+                "6",
+                "My favorite player is Bogdan Bogdanovic",
+                new Location(38.6695, -121.3140),
+                LocalDateTime.of(2018, 11, 21, 15, 00)
+        ));
+        s.add(new Rider(
+                "Iman Shumpert",
+                "7",
+                "My favorite player is Iman Shumpert",
+                new Location(38.6558, -121.3092),
+                LocalDateTime.of(2018, 11, 21, 15, 00)
+        ));
+        s.add(new Rider(
+                "Harry Giles",
+                "8",
+                "My favorite player is Harry Giles",
+                new Location(38.7063, -121.3124),
+                LocalDateTime.of(2018, 11, 21, 15, 00)
+        ));
+        s.add(new Rider(
+                "Buddy Hield",
+                "9",
+                "My favorite player is Buddy Hield",
+                new Location(38.6775, -121.3429),
                 LocalDateTime.of(2018, 11, 21, 15, 00)
         ));
         return s;
