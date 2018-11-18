4dbb76dfa3dab3093645e563fc235b004280cd98
Stefan Layanto <stef.layanto@gmail.com>
2018-11-18 01:30:31 -0800

    added JSONgenerator

--- src/main/java/Networks/JSONGenerateHelper.java
+++ src/main/java/Networks/JSONGenerateHelper.java
@@ -49,6 +49,38 @@ public class JSONGenerateHelper {
          ]
          }
          */
-        return null;
+        JSONObject obj = new JSONObject();
+        obj.put("driver", route.getKey());
+
+        JSONArray list = new JSONArray();
+        for (Rider r : route.getValue()) {
+            list.add(generateRider(r));
+        }
+        obj.put("riders", list);
+        return obj;
     }
+    public static JSONObject generateDriver(Driver driver) {
+        JSONObject obj = new JSONObject();
+        obj.put("user_name", driver.userName);
+        obj.put("user_id", driver.userId);
+        obj.put("lat", driver.location.latitude);
+        obj.put("lon", driver.location.longitude);
+        obj.put("from_time", "15:00");
+        obj.put("to_time", "17:00");
+        obj.put("rendezvous", driver.meetupLocation);
+        obj.put("seat_available", driver.maxSeats);
+        return obj;
+
+    }
+    public static JSONObject generateRider(Rider rider) {
+        JSONObject obj = new JSONObject();
+        obj.put("user_name", rider.userName);
+        obj.put("user_id", rider.userId);
+        obj.put("lat", rider.location.latitude);
+        obj.put("lon", rider.location.longitude);
+        obj.put("from_time", "15:00");
+        obj.put("to_time", "17:00");
+        return obj;
+    }
+
 }
